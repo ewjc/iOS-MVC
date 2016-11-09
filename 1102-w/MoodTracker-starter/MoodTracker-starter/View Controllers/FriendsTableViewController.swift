05e5ece67d09f6bfd3dc2545eb9ae78763cf58db
ewjc <ericswong7@gmail.com>
2016-11-09 14:11:32 -0800

    updated table controller

--- 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
+++ 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
@@ -9,13 +9,55 @@
 import UIKit
 
 class FriendsTableViewController: UITableViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+    
+    static let angryString = "Rinse yourself before entering"
+    static let happyString = "PTL"
+    static let mediumString = "Yes you may eat and drink"
+    
+    var friendArray = [
+        Friend.init(name: "Matthew", mood: .happy),
+        Friend.init(name: "Mark", mood: .happy),
+        Friend.init(name: "Luke", mood: .happy),
+        Friend.init(name: "John", mood: .happy)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendTableViewCell") as! FriendTableViewCell
+        
+        let friend = friendArray[indexPath.row]
+        cell.nameLabel.text = friend.name
+        
+        switch friend.mood {
+        case .happy:
+            cell.descriptionLabel?.text = FriendsTableViewController.happyString
+            cell.moodLabel.setTitle(Mood.happy.rawValue, for: .normal)
+        case .angry:
+            cell.descriptionLabel.text = FriendsTableViewController.angryString
+            cell.moodLabel.setTitle(Mood.angry.rawValue, for: .normal)
+        case .medium:
+            cell.descriptionLabel.text = FriendsTableViewController.mediumString
+            cell.moodLabel.setTitle(Mood.medium.rawValue, for: .normal)
+        }
+        
+        cell.descriptionLabel.text = FriendsTableViewController.happyString
+        cell.moodLabel.setTitle(Mood.happy.rawValue, for: .normal)
 
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
-  }
-
+        
+        return cell
+    }
+    
 }
--- 1102-w/MoodTracker-starter/MoodTracker-starter/View/FriendTableViewCell.swift
+++ 1102-w/MoodTracker-starter/MoodTracker-starter/View/FriendTableViewCell.swift
@@ -12,8 +12,13 @@ class FriendTableViewCell: UITableViewCell {
   
   var friend: Friend? // stores the friend that is displayed in this cell
   
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var moodLabel: UIButton!
+    
   @IBAction func moodButtonPressed(_ sender: UIButton) {
     print(#line, #function)
+    
   }
 
 
